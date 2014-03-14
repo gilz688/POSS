e8224d9dfdde7a55d34a63e086ab4dfc4301ba2c
root <vercilliusjrmila@gmail.com>
2014-03-14 13:09:10 +0800

    added PurchasedItemRepository

--- app/models/PurchasedItemsRepository.php
+++ app/models/PurchasedItemsRepository.php
@@ -1,25 +1,98 @@
 <?php
 
-class PurchasedItemsRepository implements TableRepository{
+class PurchasedItemsRepository implements TableRepository {
 
-    public function add($attributes) {
-        // some code here
+	 protected static $writePermissions = [
+        'admin' => true,
+        'auditor' => false,
+        'clerk' => false,
+        null => false
+    ];
+    
+    protected static $readPermissions = [
+        'admin' => true,
+        'auditor' => true,
+        'clerk' => true,
+        null => false
+    ];
+
+    public function checkWritePermissions() {
+        $role = Auth::user()->role;
+        if (self::$writePermissions[$role] != true) {
+            throw new UnauthorizedException('Write access to table repository is denied!');
+        }
+    }
+    
+    public function checkReadPermissions() {
+        $role = Auth::user()->role;
+        if (self::$readPermissions[$role] != true) {
+            throw new UnauthorizedException('Read access to table repository is denied!');
+        }
     }
 
-    public function delete($id) {
-        throw new IllegalOperationException('Editing transactions is not allowed!');
+    public function add($attributes) {
+        $this->checkWritePermissions();
+        $rules = [
+            'barcode' => 'required',
+            'quantity' => 'required',
+            'transaction_id' => 'required'];
+        
+        $validator = Validator::make($attributes, $rules);
+        if ($validator->passes()) {
+            $purchasedItem = new PurchasedItem;
+            $purchasedItem->barcode = $attributes['barcode'];
+            $purchasedItem->quantity = $attributes['quantity'];
+            $purchasedItem->transaction_id = $attributes['transaction_id'];
+            $purchasedItem->save();
+            return $purchasedItem;
+        } else {
+            throw new ErrorException("Invalid data!");
+        }
+    }
+
+    public function delete($barcode) {
+		$this->checkWritePermissions();
+		$item = PurchasedItem::find($barcode);
+		if($item != null){
+			$item->delete();
+		}
+		else{
+			throw new ErrorException("Invalid Barcode");
+		}
+       
     }
 
     public function all() {
-        // some code here
-        return PurchasedItem::all();
+		$this->checkReadPermissions();
+        return PurchasedItem::orderBy('transaction_id')->get();
     }
 
     public function edit($id, $attributes) {
-        throw new IllegalOperationException('Editing transactions is not allowed!');
+		$this->checkWritePermissions();
+		$item = PurchasedItem::find($id);
+		if($id == null){
+			throw new IllegalOperationException('Invalid Barcode');}
+		else {
+			if(array_key_exists('quantity',$attributes)){
+				$quantity = $attributes['quantity'];
+				if(gettype($quantity) == 'integer'){
+					$item->quantity = $attributes['quantity'];
+				}
+				else{
+					throw new ErrorException('Quantity should be an integer.! ');
+				}
+			}
+			$item->update();
+		}
     }
-    
+
     public function find($id) {
-        // some code here
+        $item = PurchasedItem::find($id);
+        if ($item == null) {
+            return null;
+        } else {
+            return $item->attributesToArray();
+        }
     }
+
 }
