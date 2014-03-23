a6768b561c2c1d3e3a819257264b078efa14be89
shinetot <shinepodiotan.iit@gmail.com>
2014-03-22 19:38:07 -0700

    final delete-confirmation modal, cancel button for edit item

--- app/views/item/edit.blade.php
+++ app/views/item/edit.blade.php
@@ -19,9 +19,7 @@
     <div class="form-group">
         <label class="col-md-4 control-label" for="barcode"><span style="font-family:sans-serif; font-size:13px ">Barcode</span></label>
         <div class="col-md-8">
-<<<<<<< HEAD
-            <input id="barcode" name="barcode" type="text" placeholder="" value="{{ $barcode }}" class="form-control input-md" />
-=======
+            
             <canvas id="ean" width="200" height="100">
                 {{ $barcode }}
             </canvas>
@@ -29,7 +27,6 @@
             <script type="text/javascript">
                 $("#ean").EAN13("{{ $barcode }}");
             </script>
->>>>>>> 31cd22a6319227c55e1d9d2e9d1743e94eab75dd
         </div>
     </div>
 
@@ -77,16 +74,7 @@
 
         </div>
     </div>
-    
-<<<<<<< HEAD
-    <!-- Uneditable category_id -->
-    <div class="form-group">
-        <label class="col-md-4 control-label" for="category_id"><span style="font-family:sans-serif; font-size:13px ">Category ID</span></label>
-        <div class="col-md-8">
-            <input  name="category_id" type="text" placeholder="" value="{{ $category_id }}" class="form-control input-md" />
-        </div>
-    </div>
-=======
+   
     	<!-- Text input-->
 <div class="form-group">
   <label class="col-md-4 control-label" for="category_id">Category</label>  
@@ -106,14 +94,14 @@
 				</select>    
   </div>
 </div>
->>>>>>> 31cd22a6319227c55e1d9d2e9d1743e94eab75dd
     
     <!-- Button -->
     <div class="form-group">
         <label class="col-md-9 control-label" for="update"></label>
         <div class="col-md-3">
             <button id="edit" name="update" class="btn btn-primary"><i class="glyphicon glyphicon-ok"></i>   UPDATE</button>
-        </div>
+			 <a href="{{ URL::route('items.index') }}" class="btn btn-large">Cancel</a>
+		</div>
     </div>
     
 </fieldset>
