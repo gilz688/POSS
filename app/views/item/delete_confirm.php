993efbdbb04828887d88c3b3fa781ae1cc3d51af
shinetot <shinepodiotan.iit@gmail.com>
2014-03-25 16:51:11 -0700

    bug fix for deleteitem confirmation modal

--- app/views/item/delete_confirm.php
+++ app/views/item/delete_confirm.php
@@ -1,5 +1,5 @@
 
-<!-- Modal Dialog --
+<!-- Modal Dialog -->
 <div class="modal fade" id="confirmDelete" role="dialog" aria-labelledby="confirmDeleteLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
@@ -19,7 +19,7 @@
 </div>
 
 
-<!-- Dialog show event handler 
+<!-- Dialog show event handler -->
 <script type="text/javascript">
   $('#confirmDelete').on('show.bs.modal', function (e) {
       $message = $(e.relatedTarget).attr('data-message');
@@ -32,23 +32,8 @@
       $(this).find('.modal-footer #confirm').data('form', form);
   });
 
-<!-- Form confirm (yes/ok) handler, submits form --
+<!-- Form confirm (yes/ok) handler, submits form -->
   $('#confirmDelete').find('.modal-footer #confirm').on('click', function(){
       $(this).data('form').submit();
   });
-</script> -->
-
-<script type="text/JavaScript">
-$(document).ready(function() {
-	$('a[data-confirm]').click(function(ev) {
-		var href = $(this).attr('href');
-		if (!$('#dataConfirmModal').length) {
-			$('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
-		} 
-		$('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
-		$('#dataConfirmOK').attr('href', href);
-		$('#dataConfirmModal').modal({show:true});
-		return false;
-	});
-});
 </script>
