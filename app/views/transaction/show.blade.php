9e70fcb7ee4c682744fc4f072ebb78cdb66e2ff4
shinetot <shinepodiotan.iit@gmail.com>
2014-03-26 11:30:29 -0700

    updates

--- app/views/transaction/show.blade.php
+++ app/views/transaction/show.blade.php
@@ -42,11 +42,9 @@
             
                 @if(Auth::user()->role == 'admin')
                 {{ Form::open(['url' => 'transactions/' . $item['barcode'], 'style' => 'float: left;']) }}
-                {{ Form::hidden('_method', 'DELETE') }}
-                {{ Form::submit('remove', ['class' => 'btn btn-danger']) }}
+                
                 {{ Form::close() }}
                 &nbsp;
-                <a class="btn btn-small btn-success" href="{{ URL::route('items.edit',$item['barcode']) }} ">edit</a>
                 @endif
             </td>
             @endforeach
